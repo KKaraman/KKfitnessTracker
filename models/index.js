c4841d1bec25dc498df2a0d57aefab2ca4a8cc42
Kerem Karaman <keremukaraman@gmail.com>
2020-11-18 18:14:54 -0600

    remove cardio.js

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-    Cardio: require("./cardio.js"),
-    Resistance: require("./resistance.js"),
+    // Cardio: require("./cardio.js"),
+    // Resistance: require("./resistance.js"),
     Workout: require("./workout.js")
 };
